7d42ba3985dec6e23b97b070acbde097f737e9a0
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-01-03 20:55:06 -0800

    completed ds-prep exc

---
+++ ds-prep-example/example.py
@@ -0,0 +1,17 @@
+print("""
+  ===============
+  ds-prep-example
+  ===============
+""")
+
+print("I would like to analyze {} to learn/predict {}.\n".format(
+
+  # write what you would like to analyze in the quotes here 👇
+
+  "anything related to nba statistics",
+
+  # write what you would like to learn from that analysis here 👇
+
+  "how players fare up against specific teams and where they score best"
+
+))
