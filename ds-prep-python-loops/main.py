079610b0349de18df97b8e9a0409898fe8c20dd7
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-01-10 20:55:57 -0800

    complete loops

---
+++ ds-prep-python-loops/main.py
@@ -0,0 +1,58 @@
+#ds-prep-python-loops
+
+record = (1, "Grimdiana", "Bones", "boulders")
+row = ""
+
+for x in record:
+  row = row + str(x) + ","
+print(row)
+
+values_list = [1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+for x in values_list:
+  print(x)
+
+index_list = []
+
+for i in range(len(values_list)):
+  index_list.insert(0, i)
+print(index_list)
+print(len(index_list) == len(values_list))
+
+for val in index_list:
+  if val % 2 != 0:
+    index_list.remove(val)
+print(index_list)
+print(values_list)
+
+vowels = {"A", "E", "I", "O", "U"}
+parts_of_the_big_letter = {"L", "M", "N", "O", "P"}
+
+for x in vowels:
+  if x in parts_of_the_big_letter:
+    parts_of_the_big_letter.remove(x)
+print(parts_of_the_big_letter)
+
+player_positions = {
+  "Who": "1B",
+  "What": "2B",
+  "I Don't Know": "3B",
+  "Why": "LF",
+  "Because": "CF",
+  "Tomorrow": "P",
+  "Today": "C",
+  "I Don't Care": "SS"
+}
+
+players = []
+for x in player_positions.keys():
+  players.append(x)
+print(players)
+
+positions = []
+for x in player_positions.values():
+  positions.append(x)
+print(positions)
+
+for x in player_positions.items():
+  print(x[0] + " " + "is on" + " " + x[1])
